f5dbe65c5d380ab672336472f09ec8869a61968e
Gabriel Brito <gabrielbrito@hubert.com.br>
2021-05-19 18:45:16 -0300

    Adicionar arquivos de projeto.

---
+++ AvaliacaoTorneio/Controllers/LutadoresController.cs
@@ -0,0 +1,91 @@
+﻿using AvaliacaoTorneio.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AvaliacaoTorneio.Controllers
+{
+    public class LutadoresController : Controller
+    {
+
+        string caminho = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("ApiTorneio")["Caminho"];
+        string key = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("ApiTorneio")["key"];
+        IEnumerable<Lutador> lutadores = null;
+
+
+        public IActionResult Index()
+        {
+            lutadores = GetLutadores();
+
+            if (lutadores != null)
+            {
+                return View(lutadores);
+            }
+            else
+            {
+                return NotFound("Erro no servidor. Contate o Administrador.");
+            }
+
+        }
+
+
+        public async Task<ActionResult> IniciarLutas()
+        {
+
+            TorneioLuta lutador = new TorneioLuta();
+
+            var lutadores = GetLutadores();
+            if (lutadores.Count() > 0)
+            {
+                var inicio = lutador.Inicio(lutadores);
+                var oitavas = lutador.VerificarGanhador(inicio);
+                var quartas = lutador.VerificarGanhador(oitavas);
+                var semifinal = lutador.VerificarGanhador(quartas);
+                var vencedor = lutador.Final(semifinal);
+
+                return View(vencedor);
+            }
+            else
+            {
+                return NotFound("Não há lutadores para iniciar a partida.");
+            }
+
+        }
+        public IEnumerable<Lutador> GetLutadores()
+        {
+
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(caminho);
+
+                client.DefaultRequestHeaders.Add("x-api-key", key);
+
+
+                var responseTask = client.GetAsync("api/competidores");
+
+
+                responseTask.Wait();
+                var result = responseTask.Result;
+
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<TorneioLuta>>();
+                    readTask.Wait();
+                    lutadores = readTask.Result;
+                }
+
+            }
+
+            return lutadores;
+        }
+
+
+
+
+    }
+}
---
+++ AvaliacaoTorneio/Models/Lutador.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AvaliacaoTorneio.Models
+{
+    public abstract class Lutador
+    {
+
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public int Idade { get; set; }
+
+        public List<string> ArtesMarciais { get; set; }
+
+        public int Lutas { get; set; }
+
+        public int Derrotas { get; set; }
+
+        public int Vitorias { get; set; }
+
+        public abstract IEnumerable<Lutador> VerificarGanhador(IEnumerable<Lutador> lutadores);
+
+
+    }
+
+    public class TorneioLuta : Lutador
+    {
+
+        public IEnumerable<Lutador> Inicio(IEnumerable<Lutador> lutadores)
+        {
+           
+          return lutadores.OrderBy(x => x.Idade).Where(x => x.Vitorias >= 24 && x.Lutas >= 24).Take(16);
+         
+          
+        }
+
+
+        public override IEnumerable<Lutador> VerificarGanhador(IEnumerable<Lutador> lutadores)
+        {
+
+            List<Lutador> resultadoGanhador = new List<Lutador>();
+            List<Lutador> resultadoLuta = new List<Lutador>();
+            foreach (var item in lutadores)
+            {
+               
+                resultadoLuta.Add(item);
+
+                if (resultadoLuta.Count()==2)
+                {
+                  
+                    resultadoGanhador.Add(resultadoLuta.OrderByDescending(x => x.Lutas).First() );
+                    resultadoLuta.Clear();
+                    
+                }
+                
+            }
+
+            return resultadoGanhador;
+        }
+
+
+
+        public Lutador Final(IEnumerable<Lutador> lutadores)
+        {
+
+
+            return lutadores.OrderByDescending(x => x.Lutas).First();
+        }
+
+
+    }
+}
